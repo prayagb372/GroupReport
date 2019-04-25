6f2556ba9f93841ad9b4ad3906c431b4c0ffc6bb
Shaunthehugo <shaun.dougherty@icloud.com>
2019-04-25 13:58:41 -0400

    Changing CSV spot and modifying main java file

--- JavaProject/AccountingReport/src/accountingreport/AccountingReport.java
+++ JavaProject/AccountingReport/src/accountingreport/AccountingReport.java
@@ -9,13 +9,27 @@ package accountingreport;
  *
  * @author pzp12
  */
+
+import com.opencsv.CSVReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AccountingReport {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
+        File output = new File("accountingReport.txt");
+        CSVReader reader = new CSVReader(new FileReader("CSV/SalesJan2009.csv"));
+        String [] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+           // nextLine[] is an array of values from the line
+           System.out.println(nextLine[0] + nextLine[1] + "etc...");
+        }
     }
     
 }
